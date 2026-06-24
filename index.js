0abdcbb17ceb601e13d8d09eb424e789df1e8560
Fábio Corleth <fabiocorleth@Fabios-MacBook-Air.local>
2026-06-24 13:18:13 -0300

    feat: initialize project, install `express` and `nodemon`

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Server running on port ${PORT}`);
+});
